81dac7ae1d03936915dd0e7d79b8d0ed8765566b
fenghaidong <fenghaidong@shizhuang-inc.com>
2025-04-22 10:28:22 +0800

    模型性能优化-知识蒸馏

--- examples/python/example.py
+++ examples/python/example.py
@@ -1,3 +1,23 @@
-from transformers import AutoModel,AutoModelForSequenceClassification
-fintuned_model_name = f'{base_model_name}-finetuned-panx-{lang}/checkpoint-2502'
-model = AutoModelForSequenceClassification.from_pretrained(fintuned_model_name).to(device)
+import os
+
+from haystack_integrations.document_stores.elasticsearch import ElasticsearchDocumentStore
+from haystack import Document
+
+
+os.environ["ELASTIC_CLIENT_DISABLE_PRODUCT_CHECK"] = "true"
+
+doc_store = ElasticsearchDocumentStore(hosts="http://localhost:9200", verify_certs=False, timeout=300)
+
+from datasets import load_dataset
+
+subjqa_dataset = load_dataset('subjqa', name="electronics")
+
+import pandas as pd
+
+dfs = {split: dset.to_pandas() for split, dset in subjqa_dataset.flatten().items()}
+for split, df in dfs.items():
+    docs = [Document(content=row["context"], meta={"item_id": row["title"], "question_id": row["id"], "split": split})
+            for _, row in df.drop_duplicates(subset="context").iterrows()]
+    doc_store.write_documents(docs)
+    break
+print(f"Loaded {doc_store.count_documents()} documents")
